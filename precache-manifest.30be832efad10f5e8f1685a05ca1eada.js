1d9a6d185c7bd2ef86f4403c82c3a76c8d7e732a
hkaur008 <hargunalone@gmail.com>
2020-03-18 17:19:31 +0530

    Updates

---
+++ precache-manifest.30be832efad10f5e8f1685a05ca1eada.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c32358fb3566cbddfbd49788d1ea3498",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "70a0067095cc9733f9e5",
+    "url": "/my-app/static/css/main.1a6da587.chunk.css"
+  },
+  {
+    "revision": "b0b49c01095482d9634f",
+    "url": "/my-app/static/js/2.be0d7338.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-app/static/js/2.be0d7338.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70a0067095cc9733f9e5",
+    "url": "/my-app/static/js/main.038d9d98.chunk.js"
+  },
+  {
+    "revision": "0946f9e65b3587162f2f",
+    "url": "/my-app/static/js/runtime-main.2ca30672.js"
+  }
+]);
